d0bc0cff09af9679848af7e277f110f1944208ac
Anton <antoniom@mail.ru>
2022-03-29 18:26:59 +0300

    29.03 - 2

--- js/script.js
+++ js/script.js
@@ -1,2 +1,17 @@
 'use strict';
 
+const btns = document.getElementsByTagName('button');
+// console.log(btns); //<button>2</button>
+
+
+const hearts = document.querySelectorAll('.heart');
+console.log(hearts); // NodeList(3) [div.heart, div.heart, div.heart] массив элементов -обращаться через метод  Foreach
+// У HTMLCollection нет методов
+
+
+hearts.forEach(item => { 
+  item.style.backgroundColor = 'blue'; // item Для всех
+  hearts[0].style.backgroundColor = 'yellow'; // hearts - выборочно
+  
+ });
+
